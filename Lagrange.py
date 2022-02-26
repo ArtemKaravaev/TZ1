bf891bc86d34cb04ee15a23084737d89e8929f11
AMnkv <99616605+AMnkv@users.noreply.github.com>
2022-02-26 14:39:14 +0300

    final_version
    Все исправил, график можно крутить и все такое, чек

--- Lagrange.py
+++ Lagrange.py
@@ -1,5 +1,5 @@
 def inputForLagrange():
-    from numpy import inf
+    
     params = input('Список переменных через пробел : ')
     params = params.split(' ')
     if len(params) > 2:
@@ -7,14 +7,18 @@ def inputForLagrange():
     elif len(params) < 2 :
         print('Введено менее двух переменных')
     F = input('Функция в аналитическом виде :  ')
+    L = input('Есть ли ограничения, 1 - да; 0 нет : ')
     if L == '1':
         lim1 = input(f'Ограничения для {params[0]} через пробел :  ')
         lim2 = input(f'Ограничения для {params[1]} через пробел :  ')
     elif L =='0':
-        lim1 = (f'{-inf} {inf}')
-        lim2 = (f'{-inf} {inf}')
+        lim1 = ('999')
+        lim2 = ('999')
     else :
         return 'Ошибка ввода наличия ограничений'
+    lim1 = list(map(int, (lim1.split(' '))))
+    lim2 = list(map(int, (lim2.split(' '))))
+
     Z = input('Ограничивающая функция :  ')
     Final = {'p1': params[0],
              'p2': params[1],
@@ -28,6 +32,10 @@ def inputForLagrange():
 from sympy import *
 
 def Lagrange(dictionary):
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from matplotlib import cm
+    from matplotlib.ticker import LinearLocator
     # преобразование данных для символьного вычислнения
     from sympy.parsing.sympy_parser import parse_expr
     data = dictionary
@@ -39,7 +47,8 @@ def Lagrange(dictionary):
     p2 = data['p2']
     y = Symbol(p2)
     l = Symbol('l')
-    
+    lim1 = data['lims1']
+    lim2 = data['lims2']
     ## реализация метода
     
     dx = func.diff(p1)
@@ -65,4 +74,37 @@ def Lagrange(dictionary):
         else:
             print(i , 'требуется дополнительное исследование')    
         
-        
+    %matplotlib notebook
+
+    plt.rcParams['figure.figsize'] = (8,6)
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+    if len(lim1) == 2 :
+        xx = np.linspace(lim1[0] - 5, lim1[1] + 5, 1000)
+        yy = np.linspace(lim2[0] - 5, lim2[1] + 5, 1000)
+    
+    else:
+        xx = np.linspace(-20, 20, 1000)
+        yy = np.linspace(-20, 20, 1000)
+    
+    X, Y = np.meshgrid(xx, yy)
+
+    f = lambdify([x,y], z)
+    Z = f(X,Y)
+
+    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,alpha=0.5,
+                       linewidth=1, antialiased=False)
+    if len(lim1) == 2 :
+        coord_x_p = np.array([i[x] for i in points if (int(i[x]) >= lim1[0] and int(i[x]) <= lim1[1])]) 
+        coord_y_p = np.array([i[y] for i in points if (int(i[y]) >= lim2[0] and int(i[y]) <= lim2[1])])
+        coord_z_p = f(coord_x_p, coord_y_p)
+    else:
+        coord_x_p = np.array([i[x] for i in points]) 
+        coord_y_p = np.array([i[y] for i in points])
+        coord_z_p = f(coord_x_p, coord_y_p)
+    
+    ax.scatter3D(coord_x_p, coord_y_p, coord_z_p,c = 'yellow', s=250, alpha=1)
+    ax.set_xlabel(f'{x}')
+    ax.set_ylabel(f'{y}')
+    ax.set_zlabel(f'{z}')
+    plt.show()
